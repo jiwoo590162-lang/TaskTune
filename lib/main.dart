3a1861907cbdc64c442735a88980fa2610425e62
Carmel1215 <kth12153@gmail.com>
2025-10-04 00:20:23 +0900

    feat: 탭 전환 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int selected = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +25,7 @@ class App extends StatelessWidget {
               width: 220,
               child: Column(
                 children: [
+                  // 앱 로고
                   Container(
                     height: 64,
                     padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -30,15 +38,139 @@ class App extends StatelessWidget {
                       ],
                     ),
                   ),
-                  const Divider(height: 1),
-                  const Expanded(
-                    child: Center(child: Text('사이드바 만들 것')),
+                  const Divider(height: 1, color: Colors.grey),
+                  // 메뉴
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            MenuItem(
+                              icon: Icons.home_rounded,
+                              label: '홈',
+                              selected: selected == 0,
+                              onTap: () => setState(() => selected = 0),
+                            ),
+                            MenuItem(
+                              icon: Icons.bar_chart_rounded,
+                              label: '통계',
+                              selected: selected == 1,
+                              onTap: () => setState(() => selected = 1),
+                            ),
+                          ],
+                        ),
+                        // 설정
+                        Column(
+                          children: [
+                            MenuItem(
+                              icon: Icons.settings_rounded,
+                              label: '설정',
+                              selected: selected == 2,
+                              onTap: () => setState(() => selected = 2),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-            const Divider(),
+            Container(
+              width: 1,
+              color: Colors.grey,
+            ),
+            // 메인 화면
+            Expanded(
+              child: IndexedStack(
+                index: selected,
+                children: [
+                  const HomePage(),
+                  const StatsPage(),
+                  const SettingsPage(),
+                ],
+              ),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('홈페이지'),
+    );
+  }
+}
+
+class StatsPage extends StatelessWidget {
+  const StatsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('통계 페이지'),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('설정 페이지'),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final bool selected;
+  final VoidCallback onTap;
+
+  const MenuItem({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.selected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: selected ? Colors.grey.shade200 : Colors.transparent,
+      child: InkWell(
+        onTap: onTap,
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+        child: SizedBox(
+          height: 50,
+          child: Center(
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 20,
+                ),
+                Icon(icon),
+                const SizedBox(
+                  width: 20,
+                ),
+                Text(label),
+              ],
+            ),
+          ),
         ),
       ),
     );
