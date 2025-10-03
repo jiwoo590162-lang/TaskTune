2dced22836605dae7c6f31f625c84e1ebe7de7ac
Carmel1215 <kth12153@gmail.com>
2025-10-03 23:16:44 +0900

    feat: 사이드바 추가

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,38 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: Row(
+          children: [
+            // 사이드바
+            SizedBox(
+              width: 220,
+              child: Column(
+                children: [
+                  Container(
+                    height: 64,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    alignment: Alignment.centerLeft,
+                    child: const Row(
+                      children: [
+                        Icon(Icons.apps),
+                        SizedBox(width: 6),
+                        Text('TaskTune'),
+                      ],
+                    ),
+                  ),
+                  const Divider(height: 1),
+                  const Expanded(
+                    child: Center(child: Text('사이드바 만들 것')),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(),
+          ],
+        ),
+      ),
+    );
   }
 }
