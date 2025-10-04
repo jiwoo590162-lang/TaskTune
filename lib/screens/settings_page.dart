e419898c1668fdf06dde813abbcf3e791a0ce876
Carmel1215 <kth12153@gmail.com>
2025-10-04 14:35:42 +0900

    feat: 세팅 버튼 추가

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -5,8 +5,55 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('설정 페이지'),
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            _SettingsItem(
+              title: 'test',
+            ),
+            _SettingsItem(
+              title: 'test',
+            ),
+            _SettingsItem(
+              title: 'test',
+            ),
+            _SettingsItem(
+              title: 'test',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SettingsItem extends StatefulWidget {
+  final String title;
+
+  const _SettingsItem({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  State<_SettingsItem> createState() => _SettingsItemState();
+}
+
+class _SettingsItemState extends State<_SettingsItem> {
+  bool _isOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+      title: Text(widget.title),
+      value: _isOn,
+      onChanged: (bool value) {
+        setState(() {
+          _isOn = value;
+        });
+      },
     );
   }
 }
